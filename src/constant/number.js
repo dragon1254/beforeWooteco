e53316e882e09b3485245db2ba4cb4d746faa701
unknown <dkvz@naver.com>
2023-12-14 15:12:30 +0900

    주의사항 추가

--- src/constant/number.js
+++ src/constant/number.js
@@ -1,4 +1,5 @@
 const NUMBER = {
+    lottoPrice : 1000,
     winPrice : [5000,50000,1500000,30000000,2000000000]
 }
 
--- src/domain/model/validate/validatePrice.js
+++ src/domain/model/validate/validatePrice.js
@@ -1,12 +1,13 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 import ERROR from "../message/error";
+import NUMBER from "../../../constant/number";
 
 class validatePrice{
     price(priceNumber){
         if(isNaN(priceNumber)){
             throw new Error(MissionUtils.Console.print(ERROR.NaN))
         }
-        if(priceNumber % 1000){
+        if(priceNumber % NUMBER.lottoPrice){
             throw new Error(MissionUtils.Console.print(ERROR.thousand))
         }
     }
